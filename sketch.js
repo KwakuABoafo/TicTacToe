979e15e616af936194a909ff2c681bbad9dfcbbe
Kwaku <boafo819@gmail.com>
2018-07-17 17:11:48 -0400

    working on ticTacToe

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup(){
+    createCanvas(900 , 900);
+}
+function draw(){
+    //horizontal lines 
+    
+    rect(0 , 300 , 300 , 300);
+    rect(0 , 600 , 300 , 300);
+    rect(0 , 900 , 300 , 300);
+
+    //vertical lines 
+    fill(0);
+    rect(300 , 900 , 300, 300 );
+    rect(600 , 900 ,  300, 300 );
+    
+}
+
+function mouseClicked(){
+
+}
